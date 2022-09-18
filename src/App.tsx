1409ece733bed1fd799146871f02aa74ac41e6ee
Pedro Paulo <pedropaulobrasca@gmail.com>
2022-09-18 17:06:42 -0300

    Criar os anuncios

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import * as Dialog from "@radix-ui/react-dialog";
+import axios from "axios";
 
 import "./styles/main.css";
 
@@ -7,10 +8,9 @@ import { GameBanner } from "./components/GameBanner";
 import { CreateAdBunner } from "./components/CreateAdBanner";
 
 import logoImg from "./assets/logo-nlw-esports.svg";
-import { GameController } from "phosphor-react";
-import { Input } from "./components/Form/Input";
+import { CreateAdModal } from "./components/CreateAdModal";
 
-interface Game {
+export interface Game {
   id: number;
   title: string;
   bannerUrl: string;
@@ -21,9 +21,9 @@ function App() {
   const [games, setGames] = useState<Game[]>([]);
 
   useEffect(() => {
-    fetch("http://localhost:3333/games")
-      .then((response) => response.json())
-      .then((data) => setGames(data));
+    axios("http://localhost:3333/games").then((response) => {
+      setGames(response.data);
+    });
   }, []);
 
   return (
@@ -38,6 +38,7 @@ function App() {
         esta aqui.
       </h1>
 
+      {/* //TODO: Usar o Keen-Slider para carrosel */}
       <div className="grid grid-cols-6 gap-6 mt-16">
         {games.map((game) => (
           <GameBanner
@@ -51,124 +52,7 @@ function App() {
 
       <Dialog.Root>
         <CreateAdBunner />
-
-        <Dialog.Portal>
-          <Dialog.Overlay className="bg-black/60 inset-0 fixed" />
-
-          <Dialog.Content className="fixed bg-[#2a2643] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg mw-[480px] shadow-black">
-            <Dialog.Title className="text-3xl text-white font-black">
-              Publicar anúncio
-            </Dialog.Title>
-
-            <form className="flex flex-col gap-4">
-              <div className="flex flex-col gap-2">
-                <label htmlFor="game" className="font-semibold">
-                  Qual o game?
-                </label>
-                <Input
-                  id="game"
-                  placeholder="Selecione o game que deseja jogar"
-                />
-              </div>
-
-              <div className="flex flex-col gap-2">
-                <label htmlFor="name">Qual seu nome (ou nickname)?</label>
-                <Input id="name" placeholder="Como te chamam dentro do game?" />
-              </div>
-
-              <div className="grid grid-cols-2 gap-6">
-                <div className="flex flex-col gap-2">
-                  <label htmlFor="yearsPlaying">Joga ha quantos anos?</label>
-                  <Input id="yearsPlaying" placeholder="Tudo bem se for ZERO" />
-                </div>
-
-                <div className="flex flex-col gap-2">
-                  <label htmlFor="discord">Qual seu discord?</label>
-                  <Input id="discord" placeholder="Fulano#1234" />
-                </div>
-              </div>
-
-              <div className="flex gap-6">
-                <div className="flex flex-col gap-2">
-                  <label htmlFor="weekDays">Quando costuma jogar?</label>
-
-                  <div className="grid grid-cols-4 gap-2">
-                    <button
-                      className="w-8 h-8 rounded bg-zinc-900"
-                      title="Domingo"
-                    >
-                      D
-                    </button>
-                    <button
-                      className="w-8 h-8 rounded bg-zinc-900"
-                      title="Segunda"
-                    >
-                      S
-                    </button>
-                    <button
-                      className="w-8 h-8 rounded bg-zinc-900"
-                      title="Terca"
-                    >
-                      T
-                    </button>
-                    <button
-                      className="w-8 h-8 rounded bg-zinc-900"
-                      title="Quarta"
-                    >
-                      Q
-                    </button>
-                    <button
-                      className="w-8 h-8 rounded bg-zinc-900"
-                      title="Quinta"
-                    >
-                      Q
-                    </button>
-                    <button
-                      className="w-8 h-8 rounded bg-zinc-900"
-                      title="Sexta"
-                    >
-                      S
-                    </button>
-                    <button
-                      className="w-8 h-8 rounded bg-zinc-900"
-                      title="Sabado"
-                    >
-                      S
-                    </button>
-                  </div>
-                </div>
-
-                <div className="flex flex-col gap-2 flex-1">
-                  <label htmlFor="hoursStart">Qual horario do dia?</label>
-                  <div className="grid grid-cols-2 gap-2">
-                    <Input type="time" id="hoursStart" placeholder="De" />
-                    <Input type="time" id="hoursEnd" placeholder="Até" />
-                  </div>
-                </div>
-              </div>
-
-              <div className="mt-2 flex gap-2 text-sm">
-                <Input type="checkbox" />
-                Costumo me conectar ao chat de voz
-              </div>
-
-              <footer className="mt-4 flex justify-end gap-4">
-                <Dialog.Close
-                  type="button"
-                  className="bg-zinc-500 px-5 h-12 rounded-md font-semibold hover:bg-zinc-600"
-                >
-                  Cancelar
-                </Dialog.Close>
-                <button
-                  type="submit"
-                  className="bg-violet-500 px-5 h-12 rounded-md font-semibold flex items-center gap-3 hover:bg-violet-600"
-                >
-                  <GameController size={24} /> Encontrar duo
-                </button>
-              </footer>
-            </form>
-          </Dialog.Content>
-        </Dialog.Portal>
+        <CreateAdModal />
       </Dialog.Root>
     </div>
   );
